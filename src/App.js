fbca17fe3271fd02a5e87ae0146e6a14668f4fe5
Aimee Truong <aimtruong@ucdavis.edu>
2022-05-13 21:43:28 -0700

    initial files added with code

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        < Navigation />
+        < About />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function About(){
+    return (
+        <section className = 'my-5'>
+            <h1 id = 'about'>About me</h1>
+            <p>This is my intro</p>
+        </section>
+    );
+}
+
+export default About;
